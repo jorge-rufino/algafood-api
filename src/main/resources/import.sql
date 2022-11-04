485b9a9b4ef4903565f3b353f83b0de08c9ec321
JorgeRufinoProgrammer <jorge.rufino1986@gmail.com>
2022-11-04 11:23:15 -0300

    Criando e testando o Repository para Restaurante

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,4 @@
 insert into cozinha (nome) values ('Tailandesa');
 insert into cozinha (nome) values ('Indiana');
+insert into restaurante (nome, taxa_frete) values ('Central', 10.00);
+insert into restaurante (nome, taxa_frete) values ('La nos Meninos', 5.00);
