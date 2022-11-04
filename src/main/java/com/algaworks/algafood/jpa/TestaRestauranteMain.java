0dc0ba7dd23ac67458c335d8d4a8594f148619b0
JorgeRufinoProgrammer <jorge.rufino1986@gmail.com>
2022-11-04 14:15:06 -0300

    Mapeamento ManyToOne na classe Restaurante

--- src/main/java/com/algaworks/algafood/jpa/TestaRestauranteMain.java
+++ src/main/java/com/algaworks/algafood/jpa/TestaRestauranteMain.java
@@ -1,13 +1,10 @@
 package com.algaworks.algafood.jpa;
 
-import java.math.BigDecimal;
-
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ApplicationContext;
 
 import com.algaworks.algafood.AlgafoodApiApplication;
-import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.RestauranteRepository;
 
 public class TestaRestauranteMain {
@@ -23,41 +20,7 @@ public class TestaRestauranteMain {
 		RestauranteRepository restauranteRepository = applicationContext.getBean(RestauranteRepository.class);
 		
 		System.out.println("\nListando todos os Restaurantes:");
-		restauranteRepository.listar().stream().forEach(x -> System.out.println(x));
-		
-		System.out.println("\nAdicionando um novo restaurante:");
-		Restaurante restaurante1 = new Restaurante();
-		restaurante1.setNome("Fogao a lenha");
-		restaurante1.setTaxaFrete(new BigDecimal("3.0"));		
-		
-		Restaurante restaurante2 = new Restaurante();
-		restaurante2.setNome("Lokko Pizza");
-		restaurante2.setTaxaFrete(new BigDecimal("2.0"));
-		
-		restauranteRepository.salvar(restaurante1);
-		restauranteRepository.salvar(restaurante2);
-		
-		System.out.println("\nListando todos os Restaurantes:");
-		restauranteRepository.listar().stream().forEach(x -> System.out.println(x));
-		
-		System.out.println("\nBuscando um Restaurante por ID: (Id = 2)");		
-		System.out.println(restauranteRepository.buscarPorId(2L));
-		
-		System.out.println("\nAlterando um Restaurante por ID: (Id = 3)");
-		Restaurante restaurante = restauranteRepository.buscarPorId(3L);
-		restaurante.setNome("Colher de Pau");
-		restaurante.setTaxaFrete(new BigDecimal("7.0"));
-		restauranteRepository.salvar(restaurante);
-		
-		System.out.println("\nListando todos os Restaurantes:");
-		restauranteRepository.listar().stream().forEach(x -> System.out.println(x));
-		
-		System.out.println("\nDeletando um Restaurante:Id = 2");
-		restaurante.setId(2L);
-		restauranteRepository.deletar(restaurante);
-		
-		System.out.println("\nListando todos os Restaurantes:");
-		restauranteRepository.listar().stream().forEach(x -> System.out.println(x));
+		restauranteRepository.listar().stream().forEach(x -> System.out.println(x));		
 		
 		System.out.println();
 	}
