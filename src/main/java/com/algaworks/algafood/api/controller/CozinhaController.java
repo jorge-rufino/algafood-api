4e5fef591c0e26ba3e3d8f42ff5703b6724e9852
JorgeRufinoProgrammer <jorge.rufino1986@gmail.com>
2022-11-12 10:48:36 -0300

    Consultando Singleton Resource com GET e @PathVariable

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -3,8 +3,8 @@ package com.algaworks.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,21 +13,21 @@ import com.algaworks.algafood.domain.repository.CozinhaRepository;
 
 //Esta annotation é mais completa e implementa as annotations "@Controller" e "@ResponseBody"
 @RestController		
-@RequestMapping(value = "/cozinhas") // , produces = MediaType.APPLICATION_JSON_VALUE) -- Para definir para todos os metodos
+@RequestMapping(value = "/cozinhas") 
 public class CozinhaController {
 	
 	@Autowired
 	private CozinhaRepository repository;
 	
-//	Chama este medodo quando o "Accept" da requisição solicitar JSON
-	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	@GetMapping
 	public List<Cozinha> listar(){
 		return repository.listar();
 	}
 	
-//	Chama este medodo quando o "Accept" da requisição solicitar XML
-	@GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
-	public List<Cozinha> listar2(){
-		return repository.listar();
+	@GetMapping(value = "/{cozinhaId}")
+	//Como a "PathVariable" e a variavel long tem o mesmo nome, poderiamos deixar a annotation "@PathVariable" sem o parametro
+	public Cozinha buscarId(@PathVariable("cozinhaId") Long cozinhaId) {
+		return repository.buscarPorId(cozinhaId);
 	}
+
 }
