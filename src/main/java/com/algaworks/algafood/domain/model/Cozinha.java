9620d3dc143c69450b4e95f5bc0d3c826e477ee0
JorgeRufinoProgrammer <jorge.rufino1986@gmail.com>
2023-01-02 11:24:46 -0300

    Mapeando relacionamento bidirecional com @OneToMany

--- src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -1,10 +1,16 @@
 package com.algaworks.algafood.domain.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -24,4 +30,11 @@ public class Cozinha {
 	
 	@Column(nullable = false)
 	private String nome;	
+	
+	//O "mappedBy" recebe o nome da variavel do objeto Cozinha na classe Restaurante para fazer o vinculo
+	//"JsonIgnore" server para evitar o loop de serialização dos objetos pois Restaurante vai chamar Cozinha e Cozinha
+	//vai chamar Restaurantes e vai entrar em loop. Assim dizemos para o Spring não serializar esta lista de Restaurantes
+	@JsonIgnore
+	@OneToMany(mappedBy = "cozinha")
+	private List<Restaurante> restaurantes =  new ArrayList<>();
 }
