fd4afb477a8132188590afde6d800a7179a8638c
JorgeRufinoProgrammer <jorge.rufino1986@gmail.com>
2023-02-08 16:08:50 -0300

    Customizando a resposta padrão de ResponseEntityExceptionHandler

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -2,11 +2,13 @@ package com.algaworks.algafood.api.exceptionhandler;
 
 import java.time.LocalDateTime;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
 import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
@@ -14,48 +16,51 @@ import com.algaworks.algafood.domain.exception.NegocioException;
 
 //Esta classe captura Exceptions de todos os controladores 
 
+//Estendo esta classe, ela tratará varias exceptions internas que podemos sobrecrever os métodos
+
 @RestControllerAdvice
-public class ApiExceptionHandler {
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler{
 	
 //	Podemos capturar exceçoes de mais de uma classe, basta adicionar chaves e vírgula como argumentos do método
 	@ExceptionHandler(EntidadeNaoEncontradaException.class)
-	public ResponseEntity<?> handlerEntidadeNaoEncontradaException(EntidadeNaoEncontradaException e){
-		
-//		Exemplo de uso de objeto com a annotation "Builder"
-		Problema problema = Problema.builder()
-				.dataHora(LocalDateTime.now())
-				.mensagem(e.getMessage()).build();
-				
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(problema);
+	public ResponseEntity<?> handlerEntidadeNaoEncontradaException(EntidadeNaoEncontradaException ex, WebRequest request){		
+
+		return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.NOT_FOUND, request);
 	}
 	
 	@ExceptionHandler(EntidadeEmUsoException.class)
-	public ResponseEntity<?> handlerEntidadeEmUsoException(EntidadeEmUsoException e){
+	public ResponseEntity<?> handlerEntidadeEmUsoException(EntidadeEmUsoException ex, WebRequest request){
 		
-		Problema problema = Problema.builder()
-				.dataHora(LocalDateTime.now())
-				.mensagem(e.getMessage()).build();
-		
-		return ResponseEntity.status(HttpStatus.CONFLICT).body(problema);
+		return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.CONFLICT, request);
 	}
 	
 	@ExceptionHandler(NegocioException.class)
-	public ResponseEntity<?> handlerNegocioException(NegocioException e){
+	public ResponseEntity<?> handlerNegocioException(NegocioException ex, WebRequest request){
 		
-		Problema problema = Problema.builder()
-				.dataHora(LocalDateTime.now())
-				.mensagem(e.getMessage()).build();
-		
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(problema);
+		return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
 	}
 	
-	@ExceptionHandler(HttpMediaTypeNotSupportedException.class)
-	public ResponseEntity<?> handlerHttpMediaTypeNotSupportedException(){
+//	Este método sobrescrito é chamado toda vez que uma Exception interna é capturada
+//	Iremos fazer nossos métodos chamarem ele tb para padronizar tudo.
+	
+	@Override
+	protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers,
+			HttpStatus status, WebRequest request) {
 		
-		Problema problema = Problema.builder()
-				.dataHora(LocalDateTime.now())
-				.mensagem("O tipo de mídia não é suportado.").build();
+//		Por padrao as Exceptions internas vem com o "body" nulo
+		if (body == null) {
+			body = Problema.builder()
+					.dataHora(LocalDateTime.now())
+					.mensagem(status.getReasonPhrase()).build();
+		} 
 		
-		return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE).body(problema);
-	} 
+//		As exceptions que nós criamos, vem com body preenchido, que são as mensagens das exceções
+		else if (body instanceof String) {
+			body = Problema.builder()
+					.dataHora(LocalDateTime.now())
+					.mensagem((String) body).build();
+		}		
+		
+		return super.handleExceptionInternal(ex, body, headers, status, request);
+	}
 }
