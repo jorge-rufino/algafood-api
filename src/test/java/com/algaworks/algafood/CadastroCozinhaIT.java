a739593f3113a008ab1347e94930537e3e178e08
JorgeRufinoProgrammer <jorge.rufino1986@gmail.com>
2023-02-22 10:53:15 -0300

    Voltando ao estado inicial do BD antes da execucao de cada teste

--- src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
+++ src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
@@ -1,10 +1,13 @@
 package com.algaworks.algafood;
 
+import org.flywaydb.core.Flyway;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
@@ -15,11 +18,18 @@ class CadastroCozinhaIT {
 	@LocalServerPort
 	private int port;
 	
+	@Autowired
+	private Flyway flyway;
+	
+//	Este método é executado antes de cada método de teste
 	@BeforeEach
 	public void setUp() {
 		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
 		RestAssured.port = port;
 		RestAssured.basePath = "/cozinhas";
+		
+//		Aqui o "afterMigrate.sql" para deixar o banco de dados igual para cada teste
+		flyway.migrate();
 	}
 	
 	@Test
@@ -44,5 +54,17 @@ class CadastroCozinhaIT {
 		.then()
 			.body("", Matchers.hasSize(4))							//verifica se existem 4 objetos na resposta
 			.body("nome", Matchers.hasItems("Indiana", "Tailandesa"));	//Verifica se existem "nomes" com os valores indicados
+	}
+	
+	@Test
+	public void deveRetornarStatus201_QuandoCadastrarCozinha() {
+		RestAssured.given()
+			.body("{ \"nome\": \"Chinesa\"} ")
+			.contentType(ContentType.JSON)
+			.accept(ContentType.JSON)
+		.when()
+			.post()
+		.then()
+			.statusCode(HttpStatus.CREATED.value());
 	}
 }
