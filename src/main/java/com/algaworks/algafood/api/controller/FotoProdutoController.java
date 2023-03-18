e08d5e50f48e2e005a9a76d518c8756eebb3d874
JorgeRufinoProgrammer <jorge.rufino1986@gmail.com>
2023-03-18 09:49:40 -0300

    Endpoint de consulta de foto de produto

--- src/main/java/com/algaworks/algafood/api/controller/FotoProdutoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/FotoProdutoController.java
@@ -1,13 +1,12 @@
 package com.algaworks.algafood.api.controller;
 
 import java.io.IOException;
-import java.nio.file.Path;
-import java.util.UUID;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +16,6 @@ import org.springframework.web.multipart.MultipartFile;
 import com.algaworks.algafood.api.assembler.FotoProdutoDtoAssembler;
 import com.algaworks.algafood.api.model.FotoProdutoDto;
 import com.algaworks.algafood.api.model.input.FotoProdutoInput;
-import com.algaworks.algafood.api.model.input.FotoProdutoMultiplaInput;
 import com.algaworks.algafood.domain.model.FotoProduto;
 import com.algaworks.algafood.domain.model.Produto;
 import com.algaworks.algafood.domain.services.FotoProdutoService;
@@ -53,22 +51,28 @@ public class FotoProdutoController {
 		return fotoProdutoDtoAssembler.toDto(fotoProdutoService.salvar(novaFoto, arquivo.getInputStream()));
 	}
 	
-	@PutMapping(value = "/multiplas", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-	public void atualizarFotoMultiplas(
-			@PathVariable Long restauranteId, @PathVariable Long produtoId, @Valid FotoProdutoMultiplaInput fotos) {
-		
-		for (FotoProdutoInput foto : fotos.getFotos()) {
-			var nomeArquivo = UUID.randomUUID().toString() + "_"+ foto.getArquivo().getOriginalFilename();
-			
-			var arquivoFoto = Path.of("/Users/Jorge Rufino/Desktop/catalogo", nomeArquivo);
-		
-			try {
-				foto.getArquivo().transferTo(arquivoFoto);
-			} catch (Exception e) {
-				throw new RuntimeException();
-			}
-		}
-		
+	@GetMapping
+	public FotoProdutoDto buscarFoto(@PathVariable Long restauranteId, @PathVariable Long produtoId) {
+		FotoProduto fotoProduto = fotoProdutoService.buscarFoto(restauranteId, produtoId);		
+		return fotoProdutoDtoAssembler.toDto(fotoProduto);
 	}
+	
+//	@PutMapping(value = "/multiplas", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+//	public void atualizarFotoMultiplas(
+//			@PathVariable Long restauranteId, @PathVariable Long produtoId, @Valid FotoProdutoMultiplaInput fotos) {
+//		
+//		for (FotoProdutoInput foto : fotos.getFotos()) {
+//			var nomeArquivo = UUID.randomUUID().toString() + "_"+ foto.getArquivo().getOriginalFilename();
+//			
+//			var arquivoFoto = Path.of("/Users/Jorge Rufino/Desktop/catalogo", nomeArquivo);
+//		
+//			try {
+//				foto.getArquivo().transferTo(arquivoFoto);
+//			} catch (Exception e) {
+//				throw new RuntimeException();
+//			}
+//		}
+//		
+//	}
 
 }
--- src/main/java/com/algaworks/algafood/domain/services/FotoProdutoService.java
+++ src/main/java/com/algaworks/algafood/domain/services/FotoProdutoService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.algaworks.algafood.domain.exception.FotoProdutoNaoEncontradoException;
 import com.algaworks.algafood.domain.model.FotoProduto;
 import com.algaworks.algafood.domain.repository.ProdutoRepository;
 import com.algaworks.algafood.domain.services.FotoStorageService.NovaFoto;
@@ -55,5 +56,10 @@ public class FotoProdutoService {
 		fotoStorageService.substituir(nomeArquivoExistente, novaFoto);
 		
 		return fotoProduto;
+	}
+	
+	public FotoProduto buscarFoto(Long restauranteId, Long produtoId) {
+		return produtoRepository.findFotoById(restauranteId, produtoId)
+				.orElseThrow(() -> new FotoProdutoNaoEncontradoException(restauranteId, produtoId));
 	}
 }
