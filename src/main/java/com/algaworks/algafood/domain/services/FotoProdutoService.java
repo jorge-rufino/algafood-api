3f8ea9c1fe8ec1f3630f7e733630e57a05b5d486
JorgeRufinoProgrammer <jorge.rufino1986@gmail.com>
2023-03-18 21:20:35 -0300

    Implementando a inclusão de objetos no bucket da Amazon S3

--- src/main/java/com/algaworks/algafood/domain/services/FotoProdutoService.java
+++ src/main/java/com/algaworks/algafood/domain/services/FotoProdutoService.java
@@ -44,6 +44,7 @@ public class FotoProdutoService {
 		
 		NovaFoto novaFoto = NovaFoto.builder()
 				.nomeArquivo(fotoProduto.getNomeArquivo())
+				.contentType(fotoProduto.getContentType())
 				.inputStream(dadosArquivo)
 				.build();
 		
--- src/main/java/com/algaworks/algafood/infrastructure/service/storage/S3FotoStorageService.java
+++ src/main/java/com/algaworks/algafood/infrastructure/service/storage/S3FotoStorageService.java
@@ -2,16 +2,51 @@ package com.algaworks.algafood.infrastructure.service.storage;
 
 import java.io.InputStream;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.algaworks.algafood.core.storage.StorageProperties;
 import com.algaworks.algafood.domain.services.FotoStorageService;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 
 @Service
 public class S3FotoStorageService implements FotoStorageService{
 
+	@Autowired
+	private AmazonS3 amazonS3;
+	
+	@Autowired
+	private StorageProperties storageProperties;
+	
 	@Override
-	public void armazenarFoto(NovaFoto novafoto) {
-		
+	public void armazenarFoto(NovaFoto novaFoto) {
+		try {
+			String caminhoArquivo = getCaminhoArquivo(novaFoto.getNomeArquivo());
+						
+			var objectMetadata = new ObjectMetadata();
+			objectMetadata.setContentType(novaFoto.getContentType()); //Passamos o contentType para abrir a foto no navegador
+			
+//		Requisição que será enviada para Amazon
+			var putObjectRequest = new PutObjectRequest(
+					storageProperties.getS3().getBucket(), 
+					caminhoArquivo, 
+					novaFoto.getInputStream(), 
+					objectMetadata)
+				.withCannedAcl(CannedAccessControlList.PublicRead); //Permite o acesso a foto pois por padrão é negado.
+			
+			amazonS3.putObject(putObjectRequest);
+			
+		} catch (Exception e) {
+			throw new StorageException("Não foi possíve enviar arquivo para AmazonS3", e);
+		}
+	}
+
+//	Retorna o caminho do diretorio/bucket mais o nome da foto
+	private String getCaminhoArquivo(String nomeArquivo) {		
+		return String.format("%s/%s", storageProperties.getS3().getDiretorioFotos(),nomeArquivo);
 	}
 
 	@Override
