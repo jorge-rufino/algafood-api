05d10fc33602f7283f2ec661b09b67a5d0ea6b28
JorgeRufinoProgrammer <jorge.rufino1986@gmail.com>
2023-03-23 18:56:20 -0300

    Implementando uma requisição GET com JavaScript

---
+++ frontend/formaPagamento.js
@@ -0,0 +1,29 @@
+function consultar() {
+    $.ajax({
+        url: "http://api.algafood.local:8080/formaPagamentos",
+        type: "get",
+
+        success: function(response){            
+            preencherTabela(response);
+        }
+    });
+  }
+  
+  
+  function preencherTabela(formasPagamento) {
+    $("#tabela tbody tr").remove();
+  
+    $.each(formasPagamento, function(i, formaPagamento) {
+      var linha = $("<tr>");
+  
+      linha.append(
+        $("<td>").text(formaPagamento.id),
+        $("<td>").text(formaPagamento.descricao)
+      );
+  
+      linha.appendTo("#tabela");
+    });
+  }
+  
+  
+  $("#btn-consultar").click(consultar);
