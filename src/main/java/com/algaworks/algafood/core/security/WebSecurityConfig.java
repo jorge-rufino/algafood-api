41062f857b1151f9d429948bbcec4532832a86e2
JorgeRufinoProgrammer <jorge.rufino1986@gmail.com>
2023-04-16 19:06:58 -0300

    Configurando autenticação de usuários em memória

--- src/main/java/com/algaworks/algafood/core/security/WebSecurityConfig.java
+++ src/main/java/com/algaworks/algafood/core/security/WebSecurityConfig.java
@@ -2,47 +2,51 @@ package com.algaworks.algafood.core.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 //Classe de configuração do HttpBasic
+//Para manter a didática do curso, continuarei estendo esta classe mesmo estando depreciada
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 	
-//	Este método está depreciado, portanto usaremos o método abaixo
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+		.withUser("jorge")
+			.password(passwordEncoder().encode("123"))
+			.roles("ADMIN")
+		.and()	
+		.withUser("joao")
+			.password(passwordEncoder().encode("123"))
+			.roles("ADMIN");	
+	}
 	
-//	@Override
-//	protected void configure(HttpSecurity http) throws Exception {
-//		http.httpBasic()									//Permite HttpBasic			.
-//			.and()	
-////			.formLogin()							  		  Não queremos que apareça o Formulario para Login							
-//			.authorizeRequests()							//Autoriza requisições
-//				.antMatchers("/v1/cozinhas/**").permitAll() //Permite qualque chamada que comece com "/v1/cozinhas/"
-//				.anyRequest().authenticated()				//Qualquer requisição que esteja autenticada
-//			.and()
-//				.sessionManagement()											//Gerenciamento de Sessão
-//					.sessionCreationPolicy(SessionCreationPolicy.STATELESS)	//Proibimos o uso de "session", assim não é gerado mais "cookies"
-//			.and()
-//				.csrf().disable();							//desabilita o "csrf"
-//	}
-	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.httpBasic()									//Permite HttpBasic			.
+			.and()	
+//			.formLogin()							  		  Não queremos que apareça o Formulario para Login							
+			.authorizeRequests()							//Autoriza requisições
+				.antMatchers("/v1/cozinhas/**").permitAll() //Permite qualque chamada que comece com "/v1/cozinhas/"
+				.anyRequest().authenticated()				//Qualquer requisição que esteja autenticada
+			.and()
+				.sessionManagement()											//Gerenciamento de Sessão
+					.sessionCreationPolicy(SessionCreationPolicy.STATELESS)	//Proibimos o uso de "session", assim não é gerado mais "cookies"
+			.and()
+				.csrf().disable();							//desabilita o "csrf"
+	}
+		
+//	Criptografa e discriptografa as senhas
 	@Bean
-    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        	http.httpBasic()
-                .and()                	
-                .authorizeRequests()
-                	.antMatchers("/v1/cozinhas/**").permitAll()                	
-                .anyRequest().authenticated()
-                .and()
-				.sessionManagement()
-					.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-				.and()
-					.csrf().disable();
-
-        return http.build();
-    }
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 }
