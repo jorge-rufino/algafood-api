7e7bf977669fe52da1767bfcd3bb5ae08b2ad851
JorgeRufinoProgrammer <jorge.rufino1986@gmail.com>
2023-05-16 11:03:53 -0300

    Descrevendo parâmetros de entrada na documentação

--- src/main/java/com/algaworks/algafood/api/v1/openapi/controller/CidadeControllerOpenApi.java
+++ src/main/java/com/algaworks/algafood/api/v1/openapi/controller/CidadeControllerOpenApi.java
@@ -6,6 +6,8 @@ import com.algaworks.algafood.api.v1.model.CidadeDto;
 import com.algaworks.algafood.api.v1.model.input.CidadeInputDto;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
@@ -17,15 +19,16 @@ public interface CidadeControllerOpenApi {
 	CollectionModel<CidadeDto> listar();
 
 	@Operation(summary = "Busca uma cidade por ID")
-	CidadeDto buscarPorId(Long cidadeId);
+	CidadeDto buscarPorId(@Parameter(description = "ID de uma cidade", example = "1", required = true) Long cidadeId);
 
 	@Operation(summary = "Cadastra uma nova cidade", description = "Cadastrar uma nova cidade necessita de um Estado e um nome válido")
-	CidadeDto adicionar(CidadeInputDto cidadeInputDto);
+	CidadeDto adicionar(@RequestBody(description = "Representação de uma nova cidade", required = true) CidadeInputDto cidadeInputDto);
 
 	@Operation(summary = "Atualiza/Altera uma cidade por ID")
-	CidadeDto atualizar(Long id, CidadeInputDto cidadeInput);
+	CidadeDto atualizar(@Parameter(description = "ID de uma cidade", example = "1", required = true) Long id, 
+			@RequestBody(description = "Representação de uma cidade com dados atualizados", required = true) CidadeInputDto cidadeInput);
 
 	@Operation(summary = "Exclui uma cidade por ID")
-	void deletar(Long cidadeId);
+	void deletar(@Parameter(description = "ID de uma cidade", example = "1", required = true) Long cidadeId);
 
 }
