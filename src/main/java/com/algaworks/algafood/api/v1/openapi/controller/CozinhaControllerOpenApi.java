2a439b2654f6efc68c66f6d9ebb9aa1f680d392c
JorgeRufinoProgrammer <jorge.rufino1986@gmail.com>
2023-05-22 16:59:06 -0300

    Descrevendo documentação de endpoints de cozinhas

--- src/main/java/com/algaworks/algafood/api/v1/openapi/controller/CozinhaControllerOpenApi.java
+++ src/main/java/com/algaworks/algafood/api/v1/openapi/controller/CozinhaControllerOpenApi.java
@@ -7,7 +7,12 @@ import com.algaworks.algafood.api.v1.model.CozinhaDto;
 import com.algaworks.algafood.api.v1.model.input.CozinhaInputDto;
 import com.algaworks.algafood.core.springdoc.PageableParameter;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
@@ -19,15 +24,31 @@ public interface CozinhaControllerOpenApi {
 //	Annotation criada que contem os parametros "page", "size" e "sort"
 //	Escondemos o objeto "Pageable" para que pegue os parametro da annotation
 	
-	@PageableParameter	
+	@PageableParameter
+	@Operation(summary = "Lista as cozinhas com paginação")
 	PagedModel<CozinhaDto> listar(@Parameter(hidden = true) Pageable pageable);
 
+	@Operation(summary = "Busca uma cozinha por ID", responses = {
+			@ApiResponse(responseCode = "200"),
+			@ApiResponse(responseCode = "400", description = "ID da cozinha inválido",content = @Content(schema = @Schema(ref = "Problema"))),
+			@ApiResponse(responseCode = "404", description = "Cozinha não encontrada",content = @Content(schema = @Schema(ref = "Problema")))
+	})
 	CozinhaDto buscarId(Long cozinhaId);
 
-	CozinhaDto adicionar(CozinhaInputDto cozinhaInputDto);
+	@Operation(summary = "Cadastra uma cozinha")
+	CozinhaDto adicionar(@RequestBody(description = "Representação de uma nova cozinha", required = true) CozinhaInputDto cozinhaInputDto);
 
+	@Operation(summary = "Atualiza uma cozinha por ID", responses = {
+			@ApiResponse(responseCode = "200"),
+			@ApiResponse(responseCode = "400", description = "ID da cozinha inválido",content = @Content(schema = @Schema(ref = "Problema"))),
+			@ApiResponse(responseCode = "404", description = "Cozinha não encontrada",content = @Content(schema = @Schema(ref = "Problema")))
+	})	
 	CozinhaDto atualizar(Long cozinhaId, CozinhaInputDto cozinhaInput);
 
+	@Operation(summary = "Exclui uma cozinha por ID", responses = {
+			@ApiResponse(responseCode = "204"),
+			@ApiResponse(responseCode = "404", description = "Cozinha não encontrada",content = @Content(schema = @Schema(ref = "Problema")))
+	})
 	void deletar(Long cozinhaId);
 
 }
