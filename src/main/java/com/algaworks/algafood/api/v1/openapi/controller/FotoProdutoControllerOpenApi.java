68513c68f9af66eab3746330f607a5a0a09136bc
JorgeRufinoProgrammer <jorge.rufino1986@gmail.com>
2023-05-23 16:22:11 -0300

    Corrigindo documentação no Swagger UI para upload de arquivos

--- src/main/java/com/algaworks/algafood/api/v1/openapi/controller/FotoProdutoControllerOpenApi.java
+++ src/main/java/com/algaworks/algafood/api/v1/openapi/controller/FotoProdutoControllerOpenApi.java
@@ -9,16 +9,20 @@ import com.algaworks.algafood.api.v1.model.FotoProdutoDto;
 import com.algaworks.algafood.api.v1.model.input.FotoProdutoInput;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 
 @SecurityRequirement(name = "security_oauth")
 public interface FotoProdutoControllerOpenApi {
 
-	FotoProdutoDto atualizarFoto(Long restauranteId, Long produtoId, FotoProdutoInput fotoProdutoInput)
-			throws IOException;
+	@Operation(summary = "Atualiza a foto do produto de um restaurante")
+	FotoProdutoDto atualizarFoto(@Parameter(description = "ID do restaurante", example = "1", required = true) Long restauranteId, 
+			@Parameter(description = "ID do produto", example = "1", required = true) Long produtoId, 
+			@RequestBody(required = true) FotoProdutoInput fotoProdutoInput) throws IOException;
 
 	@Operation(summary = "Busca foto do produto de um restaurante", responses = {
 			@ApiResponse(responseCode = "200", content = {
