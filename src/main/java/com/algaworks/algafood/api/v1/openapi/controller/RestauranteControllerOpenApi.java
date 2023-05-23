6dd0b7832e8667dc72824afc8ee3db23dc5f4c78
JorgeRufinoProgrammer <jorge.rufino1986@gmail.com>
2023-05-23 10:18:54 -0300

    Descrevendo parâmetros de projeções em endpoints de consultas

--- src/main/java/com/algaworks/algafood/api/v1/openapi/controller/RestauranteControllerOpenApi.java
+++ src/main/java/com/algaworks/algafood/api/v1/openapi/controller/RestauranteControllerOpenApi.java
@@ -11,15 +11,25 @@ import com.algaworks.algafood.api.v1.model.RestauranteBasicoDto;
 import com.algaworks.algafood.api.v1.model.RestauranteDto;
 import com.algaworks.algafood.api.v1.model.input.RestauranteInputDto;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 
 @SecurityRequirement(name = "security_oauth")
 public interface RestauranteControllerOpenApi {
 
-	//	@JsonView(RestauranteView.Resumo.class)
+	@Operation(parameters = {
+		@Parameter(
+				name = "projecao", 
+				description = "Nome da projecao", 
+				example = "apenas-nome", 
+				in = ParameterIn.QUERY, 
+				required = false)	
+	})
 	ResponseEntity<CollectionModel<RestauranteBasicoDto>> listar(ServletWebRequest request);
 
-	//	@JsonView(RestauranteView.ApenasNome.class)
+	@Operation(hidden = true)
 	CollectionModel<RestauranteApenasNomeDto> listarApenasNome();
 
 	RestauranteDto buscarId(Long id);
