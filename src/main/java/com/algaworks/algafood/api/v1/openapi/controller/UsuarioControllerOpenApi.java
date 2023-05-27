5ac233f3e738660dfb7b6736970d9c7a8c143944
JorgeRufinoProgrammer <jorge.rufino1986@gmail.com>
2023-05-27 10:38:43 -0300

    Descrevendo documentação de endpoints de usuários

--- src/main/java/com/algaworks/algafood/api/v1/openapi/controller/UsuarioControllerOpenApi.java
+++ src/main/java/com/algaworks/algafood/api/v1/openapi/controller/UsuarioControllerOpenApi.java
@@ -7,21 +7,56 @@ import com.algaworks.algafood.api.v1.model.input.SenhaInputDto;
 import com.algaworks.algafood.api.v1.model.input.UsuarioComSenhaInputDto;
 import com.algaworks.algafood.api.v1.model.input.UsuarioInputDto;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @SecurityRequirement(name = "security_oauth")
+@Tag(name = "Usuários")
 public interface UsuarioControllerOpenApi {
 
+	@Operation(summary = "Lista os usuários")
 	CollectionModel<UsuarioDto> listar();
 
-	UsuarioDto buscarPorId(Long usuarioId);
+	@Operation(summary = "Busca um usuário por ID", responses = {
+			@ApiResponse(responseCode = "200"),
+			@ApiResponse(responseCode = "400", description = "ID do usuário inválido", content = {@Content(schema = @Schema(ref = "Problema")) }),
+			@ApiResponse(responseCode = "404", description = "Usuário não encontrado", content = {@Content(schema = @Schema(ref = "Problema")) }),
+	})
+	UsuarioDto buscarPorId(
+			@Parameter(description = "ID do usuário", example = "1", required = true) Long usuarioId);
 
-	UsuarioDto adicionar(UsuarioComSenhaInputDto usuarioInputComSenha);
+	@Operation(summary = "Cadastra um usuário", responses = {
+			@ApiResponse(responseCode = "201", description = "Usuário cadastrado"),
+	})
+	UsuarioDto adicionar(
+			@RequestBody(description = "Representação de um novo usuário", required = true) UsuarioComSenhaInputDto usuarioInputComSenha);
 
-	UsuarioDto atualizar(Long usuarioId, UsuarioInputDto usuarioInput);
+	@Operation(summary = "Atualiza um usuário por ID", responses = {
+			@ApiResponse(responseCode = "200", description = "Usuário atualizado"),
+			@ApiResponse(responseCode = "404", description = "Usuário não encontrado", content = {@Content(schema = @Schema(ref = "Problema")) })
+	})
+	UsuarioDto atualizar(
+			@Parameter(description = "ID do usuário", example = "1", required = true) Long usuarioId, 
+			@RequestBody(description = "Representação de um usuário com os novos dados", required = true) UsuarioInputDto usuarioInput);
 
-	void deletar(Long usuarioId);
+	@Operation(summary = "Atualiza a senha de um usuário", responses = {
+			@ApiResponse(responseCode = "204", description = "Senha alterada com sucesso"),
+			@ApiResponse(responseCode = "404", description = "Usuário não encontrado", content = {@Content(schema = @Schema(ref = "Problema")) })
+	})
+	void alterarSenha(
+			@Parameter(description = "ID do usuário", example = "1", required = true) Long usuarioId, 
+			@RequestBody(description = "Representação de uma nova senha", required = true) SenhaInputDto senhaInput);
 
-	void alterarSenha(Long usuarioId, SenhaInputDto senhaInput);
-
+	@Operation(summary = "Exclui um usuário", responses = {
+			@ApiResponse(responseCode = "204", description = "Usuário excluído com sucesso"),
+			@ApiResponse(responseCode = "404", description = "Usuário não encontrado", content = {@Content(schema = @Schema(ref = "Problema")) })
+	})
+	void deletar(
+			@Parameter(description = "ID do usuário", example = "1", required = true) Long usuarioId);
 }
